2dc7d6966fa6d33b67b91a4be290882722948e7c
datadavev <datadavev@users.noreply.github.com>
2019-08-20 08:26:02 -0600

    Added option for specifying task to run

--- tbshacl/__init__.py
+++ tbshacl/__init__.py
@@ -3,12 +3,24 @@ import subprocess
 import os
 
 
-def tbShaclValidate(data_file, shape_file=None):
-    try:
-        shacl_validate = os.path.join(os.environ["SHACLROOT"], "shaclvalidate.sh")
-    except KeyError as e:
-        logging.error("Environment variable SHACLROOT must point to the folder containing shaclvalidate.sh")
-        return None,None
+def tbShaclValidate(data_file, shape_file=None, shacl_validate=None):
+    """
+    Run the TopBraid SHACL validation tool.
+
+    Args:
+        data_file: path to data file in turtle format.
+        shape_file: path to shape file in turtle format, defaults to data_file if not specified.
+        shacl_validate: path to the shaclvalidate.sh script or equivalent. Derived from SHACLROOT if not specified.
+
+    Returns: (captured stdout, captured stderr)
+
+    """
+    if shacl_validate is None:
+        try:
+            shacl_validate = os.path.join(os.environ["SHACLROOT"], "shaclvalidate.sh")
+        except KeyError as e:
+            logging.error("Environment variable SHACLROOT must point to the folder containing shaclvalidate.sh")
+            return None,None
     args = [shacl_validate, "-datafile", data_file]
     if shape_file is not None:
         args.append("-shapesfile")
@@ -16,4 +28,5 @@ def tbShaclValidate(data_file, shape_file=None):
     completed = subprocess.run(args,capture_output=True)
     logging.debug("ARGS=" + str(completed.args))
     return completed.stdout, completed.stderr
+
 
