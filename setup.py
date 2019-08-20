eb33e03fedb31d6457ff60fe1395affde6a4345e
datadavev <datadavev@users.noreply.github.com>
2019-08-20 08:41:15 -0600

    Adjusted revision and dependencies

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0b2',
+    version='0.0.1',
 
     description='Python wrapper for TopBraid SHACL validator',
     long_description=long_description,
@@ -38,7 +38,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -68,7 +68,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[ ],
+    install_requires=[
+        'rdflib>=4.2',
+        'jinja2',
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
